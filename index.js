dd4a096009ee28e52707c26e2c880c31cb230ef1
Quinterlozi <jojobobo254@gmail.com>
2023-03-18 15:21:54 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, eventType) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventType} gift!`);
+  }
+
+  return messages;
+}
+
+console.log(writeCards(names, "surprise"));
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(10);
+
